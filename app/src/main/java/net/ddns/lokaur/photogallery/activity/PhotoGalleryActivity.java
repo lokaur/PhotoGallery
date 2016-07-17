34ccdabcb9b5ffcbd0733fb7e6af7da985cc6596
lokaur <andremartynov@gmail.com>
2016-07-17 23:05:15 +0300

    fix service

--- app/src/main/java/net/ddns/lokaur/photogallery/activity/PhotoGalleryActivity.java
+++ app/src/main/java/net/ddns/lokaur/photogallery/activity/PhotoGalleryActivity.java
@@ -1,10 +1,16 @@
 package net.ddns.lokaur.photogallery.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import net.ddns.lokaur.photogallery.fragment.PhotoGalleryFragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
